84d1bbaa41c9d2cf9bb6a737ff0e2aa28eae0e12
red-ornob <red.ornob.dev@gmail.com>
2025-01-27 20:05:29 +0600

    fix(project.py): cleanup

--- project.py
+++ project.py
@@ -1,26 +1,27 @@
-import os
-import argparse
-import chess
-import chess.engine
+import os, argparse, chess, chess.engine
 
 
-def main():
-    config = get_args()
+def main() -> None:
+    config: argparse.Namespace = get_args()
+    
     while not board.is_game_over():
+        
         if config.board:
             print(board)
+        
         board.push(get_move())
-        board.push(engine.analysis(board, chess.engine.Limit(time=config.time, depth=config.depth)).wait().move)
+        board.push(bot.analysis(board, chess.engine.Limit(time=config.time, depth=config.depth)).wait().move)
 
 
-def get_move():
+def get_move() -> chess.Move:
     while True:
         try:
             move: str = input(">").strip().lower()
+            
             if command(move):
                 continue
-            move: chess.Move = chess.Move.from_uci(move)
             
+            move: chess.Move = chess.Move.from_uci(move)
             if move in board.legal_moves:
                 return move
             print("Illegal move")
@@ -29,18 +30,7 @@ def get_move():
             print("Invalid move or command")
 
 
-def check_save():
-    try:
-        with open("autosave.chess", "r") as savefile:
-            if (fen := savefile.read()) \
-                    and input("Do you want to load the last game?(y/N) ").strip().lower().startswith("y"):
-                board.set_fen(fen)
-            os.remove("autosave.chess")
-    except FileNotFoundError:
-        pass
-
-
-def get_args():
+def get_args() -> argparse.Namespace:
     parser: argparse.ArgumentParser = argparse.ArgumentParser(prog="cli-chess", exit_on_error=False,
                                                               description="a cli chess game to play against stockfish")
     parser.add_argument("-l", "--load", type=str,
@@ -52,49 +42,77 @@ def get_args():
     parser.add_argument("-d", "--depth", default=20, type=int,
                         help="sets the depth of analysis limit for the bot,\ndefaults to 20")
     
-    args = parser.parse_args()
+    args: argparse.Namespace = parser.parse_args()
+    
     if args.load:
-        try:
-            board.set_fen(args.load)
-        except ValueError:
-            print("Bad board string")
-            raise KeyboardInterrupt
+        set_board(args.load)
     else:
-        check_save()
+        check_autosave()
+    
     return args
 
 
-def command(move: str):
+def check_autosave() -> None:
+    try:
+        with open("autosave.chess", "r") as savefile:
+            
+            # noinspection SpellCheckingInspection
+            if (fen := savefile.read()) != r"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"\
+                    and input("Do you want to load the last game?(y/N) ").strip().lower().startswith("y"):
+                set_board(fen)
+            os.remove("autosave.chess")
+    
+    except FileNotFoundError:
+        pass
+
+
+def command(move: str) -> bool:
     match move:
         case "board":
             print(board)
+        
         case "save":
             print(f"To load this later, just pass in \n{board.fen()}\n"
                   "with the [-l | --load] argument or the load in game command")
+        
         case "load":
-            try:
-                board.set_fen(input("Paste board string here: "))
-            except (ValueError, EOFError, KeyboardInterrupt):
-                return False
+            set_board(input("Paste board string here: "))
+        
+        case "quit":
+            raise KeyboardInterrupt
+        
         case _:
             return False
+    
     return True
+
+
+def set_board(fen: str) -> None:
+    try:
+        board.set_fen(fen)
+    
+    except ValueError:
+        print("Bad board string")
+        raise KeyboardInterrupt
 
 
 if __name__ == "__main__":
     board: chess.Board = chess.Board()
-    engine: None = None
+    bot: None = None
     
     try:
-        engine: chess.engine.SimpleEngine = chess.engine.SimpleEngine.popen_uci("./stockfish")
+        bot: chess.engine.SimpleEngine = chess.engine.SimpleEngine.popen_uci("./stockfish")
         main()
+    
     except (EOFError, KeyboardInterrupt):
         with open("autosave.chess", "w+") as autosave:
             autosave.write(board.fen())
+    
     except argparse.ArgumentError as err:
         print(str(err))
+    
     except FileNotFoundError:
         print("Make sure you have stockfish downloaded and added to path or the execution dir")
     
-    if engine is not None:
-        engine.quit()
+    if bot is not None:
+        bot.quit()
